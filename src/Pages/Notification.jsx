f892c06424bbb6b44270efc307b078f5bc402c78
brooktewabe <brooktewabe00@gmail.com>
2024-11-12 16:19:25 +0300

    Notification filter

--- src/Pages/Notification.jsx
+++ src/Pages/Notification.jsx
@@ -2,42 +2,33 @@ import { useState, useEffect } from "react";
 import axios from "../axiosInterceptor";
 import withAuth from "../withAuth";
 import UnauthorizedAccess from "../Components/UnauthorizedAccess";
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaFilter } from "react-icons/fa";
 
 const Notification = () => {
   const [notifications, setNotifications] = useState([]);
   const [filteredNotifications, setFilteredNotifications] = useState([]);
   const [searchVisible, setSearchVisible] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
+  const [filterVisible, setFilterVisible] = useState(false);
+  const [filterKeyword, setFilterKeyword] = useState("");
   const [dateRange, setDateRange] = useState({ start: null, end: null });
   const currentRole = localStorage.getItem("role");
 
   useEffect(() => {
     const fetchNotifications = async () => {
       try {
-        const response = await axios.get(
-          `https://api.akbsproduction.com/notification/all`
-        );
+        const response = await axios.get(`http://localhost:5000/notification/all`);
         setNotifications(response.data);
 
-        // Get the date range
         const currentDate = new Date();
         const thirtyDaysAgo = new Date();
         thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
 
-        // Set the date range in state
-        setDateRange({
-          start: thirtyDaysAgo,
-          end: currentDate,
-        });
+        setDateRange({ start: thirtyDaysAgo, end: currentDate });
 
-        // Filter notifications for the past 30 days
         const recentNotifications = response.data
-          .filter(
-            (notification) => new Date(notification.createdAt) >= thirtyDaysAgo
-          )
-          .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-          .reverse(); // Sort by createdAt descending;
+          .filter(notification => new Date(notification.createdAt) >= thirtyDaysAgo)
+          .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
         setFilteredNotifications(recentNotifications);
       } catch (error) {
@@ -46,21 +37,27 @@ const Notification = () => {
     };
 
     fetchNotifications();
-  }, [searchTerm]);
+  }, []);
 
   useEffect(() => {
-    let updatedNotifications = filteredNotifications;
+    let updatedNotifications = notifications;
 
     if (searchTerm) {
-      updatedNotifications = updatedNotifications.filter((notification) =>
+      updatedNotifications = updatedNotifications.filter(notification =>
         notification.message.toLowerCase().includes(searchTerm.toLowerCase())
       );
     }
 
-    setFilteredNotifications(updatedNotifications);
-  }, [searchTerm, filteredNotifications]);
+    if (filterKeyword) {
+      updatedNotifications = updatedNotifications.filter(notification =>
+        notification.message.toLowerCase().includes(filterKeyword.toLowerCase())
+      );
+    }
 
-  const formatDate = (date) => {
+    setFilteredNotifications(updatedNotifications);
+  }, [searchTerm, filterKeyword, notifications]);
+
+  const formatDate = date => {
     return date.toLocaleDateString("en-US", {
       year: "numeric",
       month: "long",
@@ -77,11 +74,9 @@ const Notification = () => {
               <h3 className="text-xl font-bold">Notification</h3>
             </div>
             <div className="bg-white p-6 rounded-lg shadow-md ml-6 min-w-fit">
-              {/* Display Date Range */}
               {dateRange.start && dateRange.end && (
                 <p className="text-sm text-gray-600">
-                  Showing notifications from {formatDate(dateRange.start)} to{" "}
-                  {formatDate(dateRange.end)}
+                  Showing notifications from {formatDate(dateRange.start)} to {formatDate(dateRange.end)}
                 </p>
               )}
               <div className="flex justify-between items-center mb-6">
@@ -91,6 +86,12 @@ const Notification = () => {
                     className="text-gray-600 hover:text-gray-900"
                   >
                     <FaSearch size={20} />
+                  </button>
+                  <button
+                    onClick={() => setFilterVisible(!filterVisible)}
+                    className="text-gray-600 hover:text-gray-900"
+                  >
+                    <FaFilter size={20} />
                   </button>
                 </div>
               </div>
@@ -105,29 +106,34 @@ const Notification = () => {
                 />
               )}
 
+              {filterVisible && (
+                <select
+                  value={filterKeyword}
+                  onChange={(e) => setFilterKeyword(e.target.value)}
+                  className="w-full mb-4 p-2 border border-gray-300 rounded"
+                >
+                  <option value="">Select Filter</option>
+                  <option value="credit">Credit</option>
+                  <option value="low">Low stock</option>
+                  <option value="deleted">Deleted Stock</option>
+                </select>
+              )}
+
               <table className="min-w-full bg-white">
                 <tbody>
-                  {filteredNotifications &&
-                    [...filteredNotifications]
-                      .reverse()
-                      .map((notification, index) => (
-                        <tr key={notification.id}>
-                          <td className="py-2 px-4 border-b">{index + 1}</td>
-                          <td className="py-2 px-4 border-b">
-                            {notification.message}
-                          </td>
-                          <td className="py-2 px-4 border-b">
-                            {new Date(notification.createdAt).toLocaleString(
-                              "en-US",
-                              {
-                                year: "numeric",
-                                month: "long",
-                                day: "numeric",
-                              }
-                            )}
-                          </td>
-                        </tr>
-                      ))}
+                  {filteredNotifications.map((notification, index) => (
+                    <tr key={notification.id}>
+                      <td className="py-2 px-4 border-b">{index + 1}</td>
+                      <td className="py-2 px-4 border-b">{notification.message}</td>
+                      <td className="py-2 px-4 border-b">
+                        {new Date(notification.createdAt).toLocaleString("en-US", {
+                          year: "numeric",
+                          month: "long",
+                          day: "numeric",
+                        })}
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
