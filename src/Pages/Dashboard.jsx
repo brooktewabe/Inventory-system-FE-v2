704869f51211c46f17f25270f3d77979dd02ae2d
brooktewabe <brooktewabe00@gmail.com>
2025-01-24 12:16:46 +0300

    dashboard cleanup

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -21,10 +21,6 @@ const Dashboard = () => {
 
     fetchStocks();
   }, []);
-  const formatProductId = (id) => {
-    if (id.length <= 10) return id; // Return the id if it's less than or equal to 10 characters
-    return `${id.slice(0, 3)}...${id.slice(-5)}`; // Format as 'xxxxx...xxxxx'
-  };
   return (
     <section className="bg-[#edf0f0b9] min-h-screen">
       <div className="container m-auto ">
@@ -46,44 +42,6 @@ const Dashboard = () => {
               <Chart />
             </div>
           </div>
-
-          {/*  full-width grid */}
-          {/* <div className="bg-white p-6 rounded-lg shadow-md ml-6 ">
-            <div className="flex justify-between items-center mb-6">
-              <h3 className="text-lg font-bold">Stock List</h3>
-            </div>
-
-            <table className="min-w-full bg-white">
-              <thead>
-                <tr>
-                  <td className="py-2 text-[#9aa3a7] text-sm px-4 border-b">No.</td>
-                  <td className="py-2 text-[#9aa3a7] text-sm px-4 border-b">Product ID</td>
-                  <td className="py-2 text-[#9aa3a7] text-sm px-4 border-b">Category</td>
-                  <td className="py-2 text-[#9aa3a7] text-sm px-4 border-b">Type</td>
-                  <td className="py-2 text-[#9aa3a7] text-sm px-4 border-b">Name</td>
-                  <td className="py-2 text-[#9aa3a7] text-sm px-4 border-b">Price</td>
-                  <td className="py-2 text-[#9aa3a7] text-sm px-4 border-b">Current Stock Level</td>
-                  <td className="py-2 text-[#9aa3a7] text-sm px-4 border-b">Reorder Level</td>
-                  <td className="py-2 text-[#9aa3a7] text-sm px-4 border-b">Location</td>
-                </tr>
-              </thead>
-              <tbody>
-                {stocks?.map((stock, index) => (
-                  <tr key={stock.id}>
-                    <td className="py-2 px-4 border-b">{index + 1}</td>
-                    <td className="py-2 px-4 border-b"> {formatProductId(stock.id)}</td>
-                    <td className="py-2 px-4 border-b"> {stock.Category}</td>
-                    <td className="py-2 px-4 border-b"> {stock.Type}</td>
-                    <td className="py-2 px-4 border-b"> {stock.Name}</td>
-                    <td className="py-2 px-4 border-b"> {stock.Price}</td>
-                    <td className="py-2 px-4 border-b"> {stock.Curent_stock}</td>
-                    <td className="py-2 px-4 border-b">{stock.Reorder_level}</td>
-                    <td className="py-2 px-4 border-b">{stock.Location}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div> */}
           <History />
         </div>
       </div>
