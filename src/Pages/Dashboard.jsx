fee0ec37caf4941d43e0d8190e85a3d1360838e7
brooktewabe <brooktewabe00@gmail.com>
2025-01-31 20:30:38 +0300

    Dashboard cleanup

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -6,21 +6,7 @@ import Total from "../Components/Total"
 import Chart from "../Components/Chart"
 import History from "../Components/History"
 const Dashboard = () => {
-  const [stocks, setStocks] = useState([]);
 
-  useEffect(() => {
-    const fetchStocks = async () => {
-      try {
-        const response = await axios.get(`https://api.akbsproduction.com/stock/all`);
-        setStocks(response.data.data); 
-        console.log(stocks)
-      } catch (error) {
-        console.error("Error fetching:", error);
-      }
-    };
-
-    fetchStocks();
-  }, []);
   return (
     <section className="bg-[#edf0f0b9] min-h-screen">
       <div className="container m-auto ">
@@ -28,9 +14,7 @@ const Dashboard = () => {
           <div className="bg-white p-4">
             <h3 className="text-xl font-bold">Dashboard</h3>
           </div>
-
           <div className="flex flex-col sm:grid sm:grid-cols-3 gap-6">
-
             <div className="px-6">
               <Revenue />
             </div>
